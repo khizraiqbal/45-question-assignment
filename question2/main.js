05e86f7b8a87937b34423ab4b874b95fe389fd21
khizr-iqbal <160323713+khizr-iqbal@users.noreply.github.com>
2024-06-07 18:26:52 -0700

    first commit

--- question2/main.js
+++ question2/main.js
@@ -2,4 +2,4 @@
 //Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 
 var personname = "Eric";
-console.log("hello! ".concat(personname, " would you like to learn some Python today?"));
+console.log(`hello! ${personname} would you like to learn some Python today?`);
